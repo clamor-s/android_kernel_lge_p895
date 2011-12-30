98a0ffb6ee25007e6deab16e08114fe1dcf737f1
Pavel Emelyanov <xemul@parallels.com>
2011-12-30 00:52:21 +0000

    sock_diag: Arrange sock_diag.h such that it is exportable to userspace
    Properly toss existing components around the ifdef __KERNEL__
    and include the header into the header-y target.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/sock_diag.h
+++ include/linux/sock_diag.h
@@ -1,12 +1,18 @@
 #ifndef __SOCK_DIAG_H__
 #define __SOCK_DIAG_H__
-struct sk_buff;
-struct nlmsghdr;
+
+#include <linux/types.h>
+
+#define SOCK_DIAG_BY_FAMILY 20
 
 struct sock_diag_req {
 	__u8	sdiag_family;
 	__u8	sdiag_protocol;
 };
+
+#ifdef __KERNEL__
+struct sk_buff;
+struct nlmsghdr;
 
 struct sock_diag_handler {
 	__u8 family;
@@ -23,4 +29,5 @@ int sock_diag_check_cookie(void *sk, __u32 *cookie);
 void sock_diag_save_cookie(void *sk, __u32 *cookie);
 
 extern struct sock *sock_diag_nlsk;
+#endif /* KERNEL */
 #endif
